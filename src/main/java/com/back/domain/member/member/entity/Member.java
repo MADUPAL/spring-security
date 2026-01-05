333b95f5c21f483c658e192eb0e31daaac2d3438
MADUPAL <kevin488@naver.com>
2026-01-05 12:52:13 +0900

    ~14114 26강

--- src/main/java/com/back/domain/member/member/entity/Member.java
+++ src/main/java/com/back/domain/member/member/entity/Member.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 
 @Entity
 @Getter
@@ -17,10 +19,14 @@ public class Member extends BaseEntity {
     private String password;
     private String nickname;
 
+    @Column(unique = true)
+    private String apiKey;// username, pw대신 쓸 난수
+
     public Member(String username, String password, String nickname) {
         this.username = username;
         this.password = password;
         this.nickname = nickname;
+        this.apiKey = UUID.randomUUID().toString();
     }
 
     //나중에 바뀔 것 같은 부분을 함수로 빼자
